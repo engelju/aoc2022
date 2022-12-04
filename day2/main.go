40fa47f8b4cd4b30ae556f36008d3954f9a225a8
Julie Engel <julie.engel@mail.com>
2022-12-04 15:08:53 +0100

    solve day2 part2

--- day2/main.go
+++ day2/main.go
@@ -17,7 +17,7 @@ func main() {
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 
-	var score int
+	var totalScore int
 
 	for scanner.Scan() {
 		var line = scanner.Text()
@@ -25,10 +25,11 @@ func main() {
 		var ownHand = string(line[2])
 		fmt.Printf("Opponent: %s vs Own: %s\n", opponentHand, ownHand)
 
-		score += getScoreForRound(opponentHand, ownHand)
+		score := getScoreForRound(opponentHand, ownHand)
 		fmt.Printf("Score: %d\n", score)
+		totalScore += score
 	}
-	fmt.Printf("Total score: %d\n", score)
+	fmt.Printf("Total score: %d\n", totalScore)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -48,30 +49,30 @@ func getScoreForRound(opponentHand, ownHand string) int {
 	switch opponentHand {
 	case "A": // rock
 		switch ownHand {
-		case "X": // rock
-			return draw + rock
-		case "Y": // paper
-			return won + paper
-		case "Z": // scissor
-			return lost + scissor
+		case "X": // lose
+			return scissor + lost
+		case "Y": // draw
+			return rock + draw
+		case "Z": // win
+			return paper + won
 		}
 	case "B": // paper
 		switch ownHand {
-		case "X": // rock
-			return lost + rock
-		case "Y": // paper
-			return draw + paper
-		case "Z": // scissor
-			return won + scissor
+		case "X": // lose
+			return rock + lost
+		case "Y": // draw
+			return paper + draw
+		case "Z": // win
+			return scissor + won
 		}
 	case "C": // scissor
 		switch ownHand {
-		case "X": // rock
-			return won + rock
-		case "Y": // paper
-			return lost + paper
-		case "Z": // scissor
-			return draw + scissor
+		case "X": // lose
+			return paper + lost
+		case "Y": // draw
+			return scissor + draw
+		case "Z": // win
+			return rock + won
 		}
 	}
 	return 0
